0bdc86a70caaf884e1be78478edbcf9a8f2a888a
lev-nabiulin <levnabiulin@yandex.ru>
2022-06-28 18:39:50 +0500

    wtf

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -63,11 +63,6 @@ class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
     permission_classes = (IsAuthorOrModerator,)
 
-    def get_queryset(self):
-        title = get_object_or_404(Title, id=self.kwargs.get("title_id"))
-        queryset = title.reviews.order_by("id")
-        return queryset
-
     def perform_create(self, serializer):
         title = get_object_or_404(Title, id=self.kwargs.get("title_id"))
         serializer.save(author=self.request.user, title=title)
@@ -76,11 +71,6 @@ class ReviewViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (IsAuthorOrModerator,)
-
-    def get_queryset(self):
-        review = get_object_or_404(Review, id=self.kwargs.get("review_id"))
-        queryset = review.comments.order_by("id")
-        return queryset
 
     def perform_create(self, serializer):
         review = get_object_or_404(Review, id=self.kwargs.get("review_id"))
